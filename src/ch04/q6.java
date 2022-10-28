864da615999c43cff233b83e89af64412cf46d8d
admin <admin@PC04>
2022-10-28 09:12:06 +0900

    구조 다시만듬 . 그리고 6번 문제 풀음

---
+++ src/ch04/q6.java
@@ -0,0 +1,15 @@
+
+public class q6 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 6; i++) {
+			for (int j = 1; j <=6; j++) {
+				if(i+j == 6) {
+					System.out.println("x = " +i +" y = "+j);
+				}
+			}
+		}
+
+	}
+
+}
